5e93055453c29a3f779181d084470ce7b779f373
Dan Picton <daniel.picton@gmail.com>
2019-08-26 20:53:07 +0100

    Move old PyQt4 stuff to PyQt5

---
+++ py_hud.py
@@ -0,0 +1,54 @@
+
+import sys
+
+from PyQt5 import QtGui, QtCore, QtWebEngine
+from PyQt5.QtWidgets import QStyle, qApp, QApplication
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+# from PyQt5.QtWidgets import
+
+html = '''
+<html>
+<head>
+<title>A Sample Page</title>
+</head>
+<h1>Hello, World!</h1>
+<hr />
+I have nothing to say.
+</body>
+</html>
+'''
+
+class myWebView(QWebEngineView):
+    def __init__(self):
+        QWebEngineView.__init__(self)
+        #self.load(image)
+        self.setHtml(html)
+        self.setWindowOpacity(0.7)
+        radius = 10.0
+        path = QtGui.QPainterPath()
+        self.resize(640,480)
+        path.addRoundedRect(QtCore.QRectF(self.rect()), radius, radius)
+        mask = QtGui.QRegion(path.toFillPolygon().toPolygon())
+        self.setMask(mask)
+        self.move(QtGui.QCursor.pos())
+        self.setWindowFlags(
+            QtCore.Qt.WindowStaysOnTopHint |
+            QtCore.Qt.FramelessWindowHint |
+            QtCore.Qt.X11BypassWindowManagerHint
+        )
+        self.setGeometry(QStyle.alignedRect(
+            QtCore.Qt.LeftToRight, QtCore.Qt.AlignCenter,
+            QtCore.QSize(640, 480),
+            qApp.desktop().availableGeometry())
+        )
+        self.setStyleSheet('QWebView{background-color: darkgray; border: 5px solid darkgray}')
+
+    def mousePressEvent(self, event):
+        qApp.quit()
+
+
+app = QApplication(sys.argv)
+browser = myWebView()
+browser.show()
+app.exec_()
