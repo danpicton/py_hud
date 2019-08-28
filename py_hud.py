f38372135e36b85f02a0d1aeb4f0685c13ff737a
Dan Picton <daniel.picton@gmail.com>
2019-08-28 20:22:17 +0100

    Add basic event handling

--- py_hud.py
+++ py_hud.py
@@ -1,7 +1,7 @@
-
 import sys
 
-from PyQt5 import QtGui, QtCore, QtWebEngine
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QStyle, qApp, QApplication
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
@@ -19,23 +19,24 @@ I have nothing to say.
 </html>
 '''
 
+
 class myWebView(QWebEngineView):
     def __init__(self):
         QWebEngineView.__init__(self)
-        #self.load(image)
+        # self.load(image)
         self.setHtml(html)
         self.setWindowOpacity(0.7)
         radius = 10.0
         path = QtGui.QPainterPath()
-        self.resize(640,480)
+        self.resize(640, 480)
         path.addRoundedRect(QtCore.QRectF(self.rect()), radius, radius)
         mask = QtGui.QRegion(path.toFillPolygon().toPolygon())
         self.setMask(mask)
         self.move(QtGui.QCursor.pos())
         self.setWindowFlags(
             QtCore.Qt.WindowStaysOnTopHint |
-            QtCore.Qt.FramelessWindowHint |
-            QtCore.Qt.X11BypassWindowManagerHint
+            QtCore.Qt.FramelessWindowHint
+            # | QtCore.Qt.X11BypassWindowManagerHint
         )
         self.setGeometry(QStyle.alignedRect(
             QtCore.Qt.LeftToRight, QtCore.Qt.AlignCenter,
@@ -43,12 +44,24 @@ class myWebView(QWebEngineView):
             qApp.desktop().availableGeometry())
         )
         self.setStyleSheet('QWebView{background-color: darkgray; border: 5px solid darkgray}')
+        self.grabMouse()
+        self.grabKeyboard()
+        print(self.keyboardGrabber())
 
     def mousePressEvent(self, event):
-        qApp.quit()
+        # qApp.quit()
+        self.close()
+        # print("click")
+
+    def keyPressEvent(self, e):
+        # print("hello")
+        if e.key() == Qt.Key_Escape:
+            self.close()
 
 
 app = QApplication(sys.argv)
 browser = myWebView()
+
 browser.show()
-app.exec_()
+# app.exec_()
+sys.exit(app.exec_())
